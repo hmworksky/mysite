c287830000c9726c10d28c9b105c1d221bdab5a9
hmworksky <hm_worksky@163.com>
2017-07-01 03:06:19 +0800

    login

--- sign/login.py
+++ sign/login.py
@@ -2,12 +2,16 @@
 from django.http import Http404,HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,redirect,render_to_response
 from django.http.response import JsonResponse
-from tool1 import *
 import os
 from models import *
 os.environ.update({"DJANGO_SETTINGS_MODULE": "config.settings"})
 
-
+def getuserid(username):
+    try :
+        user_id = Login.objects.values("id").get(username = username)["id"]
+    except Exception , e :
+	    return False
+    return user_id
 
 def register(request):
     if request.method == 'POST' :
@@ -15,14 +19,14 @@ def register(request):
         password = request.POST.get('password')
         pwdagain = request.POST.get('pwdagain')
         if password == pwdagain :
-            if Login.objects.filter(username = username):
+            if Login.objects.filter(username = username) :
+                return render_to_response('signup.html', {'errormsg': '用户名已注册'})
+            else :
                 if Login.objects.get_or_create(username=username,password=password) :
                     request.session['username']=username
                     return redirect('/tool/index/')
                 else:
                     return   render_to_response('signup.html')
-            else :
-                return render_to_response('signup.html',{'errormsg':'用户名已注册'})
         else :
             return render_to_response('signup.html',{'errormsg':'请输入相同密码'})
     return render_to_response('signup.html')
@@ -44,12 +48,14 @@ def resetlogin(request):
         password = request.POST.get('password')
         pwdagain = request.POST.get('pwdagain')
         if password == pwdagain :
-            if Login.objects.filter(username = username) is False:#判断用户名是否存在
-                Login.objects.get(username = username ).update(password = password)
+            if Login.objects.filter(username = username) :#判断用户名是否存在
+                Login.objects.filter(username = username ).update(password = password)
                 request.session['username']=username
                 return redirect('/tool/index/')
             else :
                 return render_to_response('reset.html',{'errormsg':'用户名不存在'})
         else :
             return render_to_response('reset.html',{'errormsg':'请输入相同密码'})
-    return render_to_response('reset.html')	
+    return render_to_response('reset.html')
+def index(request):
+    return HttpResponse("welcome")
