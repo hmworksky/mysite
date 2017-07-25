2ecd90638fc74bd6c6f8f513e2fdccdc91b44513
root <root@iZuf65is23zeo14kto3ym2Z.(none)>
2017-07-25 14:13:13 +0800

    c

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': 'root',  
         'PASSWORD': 'test1324',  
         'HOST': '127.0.0.1',  
-        'PORT': '33306',
+        'PORT': '3306',
 	'OPTIONS':{
 	    'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
 	    'charset': 'utf8', 
