b2f1774b9e5879203a5eb7f77f974240a0b2a1be
hmworksky <hm450482292h@163.com>
2017-07-25 13:43:59 +0800

    '13'

--- home/homepage.py
+++ home/homepage.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import  render_to_response,redirect
-from django.http import  HttpResponse,Http404,HttpResponseRedirect
+from django.http import  HttpResponse,Http404,HttpResponseRedirect,JsonResponse
 
 
 
@@ -14,3 +14,9 @@ def index(request):
 def get_create(request):
     username = request.session['username']
     return render_to_response('http_tool/get_create.html',{'username':username})
+def ajax_dict():
+    name_dict = {'衬衫': '5', '羊毛衫': '2', '雪纺衫': '36', '裤子': '10', '高跟鞋': '10', '袜子': '20'}
+    return JsonResponse(name_dict)
+def ajax_dict1():
+    name_dict = {'衬衫': '5', '羊毛衫': '2', '雪纺衫': '36', '裤子': '10', '高跟鞋': '10', '袜子': '20'}
+    return JsonResponse(name_dict)
