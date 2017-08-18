7723278b98c49528c3b5ec1b018ae2104dca4fc3
hmworksky <hm450482292h@163.com>
2017-08-18 18:52:34 +0800

    '818'

--- env_config/env_branch.py
+++ env_config/env_branch.py
@@ -12,7 +12,7 @@ def get_branch(request):
     dic['name'] = branch[0]
     dic['value'] = branch[1]
     dic = json.dumps(dic)
-    if request.is_ajax() :
+    if request.method == 'POST' :
         return HttpResponse(dic,content_type='application/json; charset= utf-8')
     else :
         raise TypeError()
--- home/homepage.py
+++ home/homepage.py
@@ -5,23 +5,28 @@ from public_tool import tools
 from urllib import unquote
 from collections import defaultdict
 import json
+from html.parser import HTMLParser
 
 
 
 
 def index(request):
-    username = request.session['username']
-    branch = tools.branch_data(5,1)
+    # username = request.session['username']
+    branch = tools.branch_data(5)
     name_value = str(branch[0]).replace("'",'"')
-    size_value = map(int,branch[1])
-    return render_to_response('index.html', locals())
+    # name_value = branch.values()
+    # size_value = map(int,branch.keys())
+    return HttpResponse(isinstance(name_value,list))
+    # parser = HTMLParser()
+    # name_value= map(parser.unescape(),name_value)
+    # return render_to_response('index.html', locals())
 
 
 def get_create(request):
     username = request.session['username']
     return render_to_response('http_tool/get_create.html',{'username':username})
 def ajax_dict():
-    name_dict = {'测试':[1,50,35,20,55]}
+    name_dict = {'测试':[1,50,35,20,56]}
     return JsonResponse(name_dict)
 def ajax_dict1(request):
     name_dict = {'衬衫': '5', '羊毛衫': '2', '雪纺衫': '36', '裤子': '10', '高跟鞋': '10', '袜子': '20'}
